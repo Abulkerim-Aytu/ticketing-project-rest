5b53ce07112a4f6299c737adc09e224a4b31dfcf
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-11-11 12:40:26 +0300

    01-api-create-update 4

---
+++ src/main/java/com/cydeo/controller/ProjectController.java
@@ -0,0 +1,53 @@
+package com.cydeo.controller;
+
+import com.cydeo.dto.ProjectDTO;
+import com.cydeo.dto.ResponseWrapper;
+import com.cydeo.service.ProjectService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/project")
+public class ProjectController {
+    private final ProjectService projectService;
+
+    public ProjectController(ProjectService projectService) {
+        this.projectService = projectService;
+    }
+
+    @GetMapping
+    public ResponseEntity<ResponseWrapper> getProjects(){
+        return ResponseEntity.ok(new ResponseWrapper("success",projectService.listAllProjects(), HttpStatus.OK));
+    };
+
+    @GetMapping("/{code}")
+    public ResponseEntity<ResponseWrapper> getProjectByCode(@PathVariable("code") String code ){
+        return ResponseEntity.ok(new ResponseWrapper("success",projectService.getByProjectCode(code), HttpStatus.OK));
+    };
+
+    @PostMapping
+    public ResponseEntity<ResponseWrapper> createProject(@RequestBody ProjectDTO project){
+        projectService.save(project);
+        return ResponseEntity
+        .status(HttpStatus.CREATED)
+        .body(new ResponseWrapper("success", HttpStatus.CREATED));
+    };
+
+    @PutMapping
+    public ResponseEntity<ResponseWrapper> updateProjects(@RequestBody ProjectDTO project){
+        return ResponseEntity.ok(new ResponseWrapper("success",projectService.update(project), HttpStatus.OK));
+    };
+
+    @DeleteMapping("/{code}")
+    public ResponseEntity<ResponseWrapper> deleteProjects(@PathVariable("code") String code){
+        projectService.delete(code);
+        return ResponseEntity.noContent().build();
+    };
+
+    @GetMapping()
+    public ResponseEntity<ResponseWrapper> getProjectByManager(){};
+
+
+    public ResponseEntity<ResponseWrapper> managerCompleteProjects(){};
+}

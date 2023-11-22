a37b936efb99927a8a7d79a4e1f54018c1002556
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-11-22 11:07:39 +0300

    03-AOP-Update 4

---
+++ src/main/java/com/cydeo/aspect/PerformanceAspect.java
@@ -0,0 +1,12 @@
+package com.cydeo.aspect;
+
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Slf4j
+// This class monitor the methods or app performance.
+public class PerformanceAspect {
+}

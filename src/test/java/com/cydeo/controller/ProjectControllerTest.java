6c46b2365b31945daa825b3b0ab084a42ee1a9e2
Abulkerim-Aytu <65733502+Abulkerim-Aytu@users.noreply.github.com>
2023-12-03 18:35:33 +0300

    04-Testing-Update 4

--- src/test/java/com/cydeo/controller/ProjectControllerTest.java
+++ src/test/java/com/cydeo/controller/ProjectControllerTest.java
@@ -147,7 +147,7 @@ class ProjectControllerTest {
 
         map.add("grant_type", "password");
         map.add("client_id", "ticketing-app");
-        map.add("client_secret", "P2tMpbaXrvkdeit4b53umI0lY1p5TtAL");
+        map.add("client_secret", "jLxFep0uT2hbDONzOHIaB1TfAHHVzhhX");
         map.add("username", "ozzy");
         map.add("password", "abc1");
         map.add("scope", "openid");
